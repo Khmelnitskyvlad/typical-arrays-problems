58b7b6f33949edde2acc563a52eff8523e6085bd
Vlad Khmelnitsky <ummmdanone@gmail.com>
2020-10-03 22:04:06 +0300

    feature: add solution to typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) return 0;
+    let minNum = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (minNum > array[i]) {
+            minNum = array[i];
+        }
+    }
+    return minNum;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) return 0;
+    let maxNum = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (maxNum < array[i]) {
+            maxNum = array[i];
+        }
+    }
+    return maxNum;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) return 0;
+    let avgNum;
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    avgNum = sum / array.length;
+    return avgNum;
+};
